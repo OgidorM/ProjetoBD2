c5a1a8322415aaeb81e5992794d184604022a9b1
spider2876 <155382150+spider2876@users.noreply.github.com>
2025-09-27 16:29:49 +0100

    Atualiza mensagem de boas-vindas em index.html do core

--- b2da1/settings.py
+++ b2da1/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'vendas_front',
     'vendaslinhas_front',
     'avaliacoes_front',
+    'sessoes_front',                
+    'categorias_front',             
+    'classificacoesetarias_front',  
+
 ]
 
 MIDDLEWARE = [
@@ -87,11 +91,11 @@ WSGI_APPLICATION = 'b2da1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cinemaDB',
-        'USER': 'admin',
+        'NAME': 'postgres',
+        'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '5434',
     }
 }
 
