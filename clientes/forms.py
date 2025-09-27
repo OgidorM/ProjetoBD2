04cf74db505f06e4c6e8e9c7ae105d094e0bfe15
OgidorM <151182218+OgidorM@users.noreply.github.com>
2025-09-27 14:47:22 +0100

    fix: ui e o delete do cliente já está com forme o resto

--- clientes/forms.py
+++ clientes/forms.py
@@ -9,4 +9,13 @@ class ClienteForm(forms.ModelForm):
             'nomecliente', 'emailcliente', 'telefonecliente', 'datanascimento',
             'moradacliente', 'codigopostalcliente', 'localidadecliente', 'nif'
         ]
-
+        widgets = {
+            'nomecliente': forms.TextInput(attrs={'class': 'form-control'}),
+            'emailcliente': forms.EmailInput(attrs={'class': 'form-control'}),
+            'telefonecliente': forms.TextInput(attrs={'class': 'form-control'}),
+            'datanascimento': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'moradacliente': forms.TextInput(attrs={'class': 'form-control'}),
+            'codigopostalcliente': forms.TextInput(attrs={'class': 'form-control'}),
+            'localidadecliente': forms.TextInput(attrs={'class': 'form-control'}),
+            'nif': forms.TextInput(attrs={'class': 'form-control'}),
+        }
