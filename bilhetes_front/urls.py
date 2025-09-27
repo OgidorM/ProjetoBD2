759f2a6d6f1fc75f5ef6065f5625e768979003a3
OgidorM <151182218+OgidorM@users.noreply.github.com>
2025-09-27 01:20:53 +0100

    Revert "Feat/severino frontend"

--- bilhetes_front/urls.py
+++ bilhetes_front/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('', views.index, name='bilhetes_index'),
     path('listar/', views.lista_bilhetes, name='lista_bilhetes'),
     path('adicionar/', views.adicionar_bilhete, name='adicionar_bilhete'),
-    path('editar/<int:bilheteid>/', views.editar_bilhete, name='editar_bilhete'), 
-    path('remover/<int:bilheteid>/', views.remover_bilhete, name='remover_bilhete'), 
 ]
--- salas_front/urls.py
+++ salas_front/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.lista_salas, name="salas_index"),
     path("listar/", views.lista_salas, name="lista_salas"),
     path("adicionar/", views.adicionar_sala, name="adicionar_sala"),
-    path("editar/<int:salaid>", views.editar_sala, name="editar_sala"),
-    path("apagar/<int:salaid>", views.remover_sala, name="apagar_sala"),
+   # path("editar/<int:sala_id>", views.editar_sala, name="sala_editar"),
+    #path("apagar/<int:sala_id>", views.apagar_sala, name="sala_apagar"),
 ]
