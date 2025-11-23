f06a110486a6dd4be3f525370a8638b9ac45d256
EmanuelFArgolo <efargolo@gmail.com>
2025-11-23 18:27:50 +0000

    hotfix for logout and blocking employees views from non users

--- b2da1/settings.py
+++ b2da1/settings.py
@@ -153,4 +153,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/' # Quando faz logout, volta ao login
+LOGOUT_REDIRECT_URL = '/accounts/login/'
+LOGIN_URL = '/accounts/login/'
